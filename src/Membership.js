6651437b092f9fa522c652af2801257479f6d580
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-05-12 18:41:23 -0400

    hello

--- src/Membership.js
+++ src/Membership.js
@@ -8,7 +8,7 @@ function Membership() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [selectedStates, setSelectedStates] = useState([]);
-    const [showPayment, setShowPayment] = useState(false); // State variable to track visibility
+    const [showPayment, setShowPayment] = useState(false); // State variable to track visibil
   
     const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'].map(state => ({ label: state, value: state }));
   
