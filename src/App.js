864583a97294bac30cb772652d58076855bdc6f6
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-06-02 21:10:45 -0400

    looking better

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LotteryData from './LotteryData';
 import Membership from './Membership';
 import Blog from './Blog';
 import PaymentForm from './PaymentForm';
-import About from './About';
+import Home from './Home'; // Import the Home component
 import Header from './Header';
 
 function App() {
@@ -23,7 +23,7 @@ function App() {
           <Route path="/lottery" element={<LotteryData />} />
           <Route path="/membership" element={<Membership onPayButtonClick={handlePayButtonClick} />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} /> {/* Added a route for the Home page */}
         </Routes>
 
         {showPaymentForm && <PaymentForm />}
--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,16 @@
-// Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'; // assuming you will have a CSS file for Header component
+import { NavLink } from 'react-router-dom';
+import './Header.css'; // Import the correct CSS file
 
 function Header() {
     return (
         <header className="header">
-            <h1>Pick3Data</h1>
             <nav>
-                <ul className="nav-links">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/lottery">Lottery Data</Link></li>
-                    <li><Link to="/membership">Membership</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul>
+                    <li><NavLink exact to="/" activeClassName="active-link">Home</NavLink></li>
+                    <li><NavLink to="/lottery" activeClassName="active-link">Lottery Data</NavLink></li>
+                    <li><NavLink to="/membership" activeClassName="active-link">Membership</NavLink></li>
+                    <li><NavLink to="/blog" activeClassName="active-link">Blog</NavLink></li>
                 </ul>
             </nav>
         </header>
@@ -21,3 +18,6 @@ function Header() {
 }
 
 export default Header;
+
+
+
