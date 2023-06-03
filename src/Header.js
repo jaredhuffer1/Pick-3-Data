07d6671dd806a35daf847a9813644f46fed4e5f6
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-06-02 21:39:46 -0400

    added mediaQueries

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css'; // Import the correct CSS file
+import './Header.css';
 
 function Header() {
-    return (
-        <header className="header">
-            <nav>
-                <ul>
-                    <li><NavLink exact to="/" activeClassName="active-link">Home</NavLink></li>
-                    <li><NavLink to="/lottery" activeClassName="active-link">Lottery Data</NavLink></li>
-                    <li><NavLink to="/membership" activeClassName="active-link">Membership</NavLink></li>
-                    <li><NavLink to="/blog" activeClassName="active-link">Blog</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <h1 className="page-name">Pick3Data</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to="/" activeClassName="active-link">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/lottery" activeClassName="active-link">
+              Lottery Data
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/membership" activeClassName="active-link">
+              Membership
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog" activeClassName="active-link">
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
-
-
-
