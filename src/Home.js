aba0f73af3a0375cf00509f6e49ddc2f2a9e44ec
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-06-07 22:55:35 -0400

    add my text in hompage

--- src/Home.js
+++ src/Home.js
@@ -12,6 +12,9 @@ function Home() {
           <li>First off, we do all the data crunching per state per drawing...daily and nightly draws and put that data in a combination format based on the highest statistical value.</li>
           <li>99% of players simply don't have the time and know-how to interpret data...it's just too troubling. Let's face it; pick3 is hard to master even for the most elite players.</li>
         </ul>
+        <p>Our perspective on state lotteries:</p>
+        <p>We've always felt that the state lotteries are greedy basically because they have cooked the odds through the payout system. 1/1000 odds with only $500 for a straight win is laughable. It's laughable because other websites, like 5dimes.com (though now unreachable), used to pay $900 for a straight played winning combination.</p>
+        <p>So if you're a true betting man, the casinos are a much better bet. Lotteries, much like liquor stores with alcoholics, prey off of people's weaknesses. That's one of the main reasons we created pick3data, to help players balance the scales back to normality. Plus, we love to see players win more...this is our goal.</p>
         <p>Common misconceptions about the lottery:</p>
         <ul>
           <li>Combinations do not follow each other. When someone tells you that combinations tend to follow previous combinations, they know absolutely nothing about statistics, and this kind of ignorant thinking is laughable.</li>
